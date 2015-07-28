b0721ed7a4faeb124c0bd9eabf6d92f9971ad481
nandaTF <nanda.pttr@gmail.com>
2015-07-28 20:39:25 +0530

    changes due to conflicts

--- Test.java
+++ Test.java
@@ -1,12 +1,1 @@
-<<<<<<< HEAD
 fghjkl
-=======
-# Created for test.jav
-# to check the triggering build
-# checking again
-# Comminting and updated
-# Checking the emial notification mails.
-
-
-# to check the build 
->>>>>>> parent of 519cad5... added CPA_test.java
